fb4c0131f18cf4c2108d694755db8a28c7b4273c
Stan Hu <stanhu@gmail.com>
2020-09-13 11:36:12 -0700

    unix: add Darwin support for clonefile syscalls
    This commit adds support for three Darwin-specific calls that accelerate
    the copying of files:
    
    * clonefile()
    * clonefileat()
    * fclonefileat()
    
    Closes https://github.com/golang/go/issues/41366

--- unix/syscall_darwin_test.go
+++ unix/syscall_darwin_test.go
@@ -4,6 +4,16 @@
 
 package unix_test
 
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	"golang.org/x/sys/unix"
+)
+
 // stringsFromByteSlice converts a sequence of attributes to a []string.
 // On Darwin, each entry is a NULL-terminated string.
 func stringsFromByteSlice(buf []byte) []string {
@@ -16,4 +26,157 @@ func stringsFromByteSlice(buf []byte) []string {
 		}
 	}
 	return result
+}
+
+func TestClonefile(t *testing.T) {
+	file, err := ioutil.TempFile("", "TestCloneFile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	data := []byte("This is a test")
+	file.Write(data)
+	file.Close()
+
+	clonedName := file.Name() + "-cloned"
+	err = unix.Clonefile(file.Name(), clonedName, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(clonedName)
+
+	clonedData, err := ioutil.ReadFile(clonedName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(data, clonedData) {
+		t.Fatal(err)
+	}
+}
+
+func TestClonefileatWithCwd(t *testing.T) {
+	file, err := ioutil.TempFile("", "TestCloneFileat")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	data := []byte("This is a test")
+	file.Write(data)
+	file.Close()
+
+	clonedName := file.Name() + "-cloned"
+	err = unix.Clonefileat(unix.AT_FDCWD, file.Name(), unix.AT_FDCWD, clonedName, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(clonedName)
+
+	clonedData, err := ioutil.ReadFile(clonedName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(data, clonedData) {
+		t.Fatal(err)
+	}
+}
+
+func TestClonefileatWithRelativePaths(t *testing.T) {
+	srcDir, err := ioutil.TempDir("", "src")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(srcDir)
+
+	dstDir, err := ioutil.TempDir("", "dest")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dstDir)
+
+	srcFd, err := unix.Open(srcDir, unix.O_RDONLY|unix.O_DIRECTORY, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer unix.Close(srcFd)
+
+	dstFd, err := unix.Open(dstDir, unix.O_RDONLY|unix.O_DIRECTORY, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer unix.Close(dstFd)
+
+	srcFile, err := ioutil.TempFile(srcDir, "TestCloneFileat")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(srcFile.Name())
+
+	dstFile, err := ioutil.TempFile(dstDir, "TestCloneFileat")
+	if err != nil {
+		t.Fatal(err)
+	}
+	os.Remove(dstFile.Name())
+
+	data := []byte("This is a test")
+	srcFile.Write(data)
+	srcFile.Close()
+
+	src := path.Base(srcFile.Name())
+	dst := path.Base(dstFile.Name())
+	err = unix.Clonefileat(srcFd, src, dstFd, dst, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	clonedData, err := ioutil.ReadFile(dstFile.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(data, clonedData) {
+		t.Fatal(err)
+	}
+}
+
+func TestFclonefileat(t *testing.T) {
+	file, err := ioutil.TempFile("", "TestCloneFile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	data := []byte("This is a test")
+	file.Write(data)
+	file.Close()
+
+	fd, err := unix.Open(file.Name(), unix.O_RDONLY, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer unix.Close(fd)
+
+	dstFile, err := ioutil.TempFile("", "TestFcloneFileat")
+	if err != nil {
+		t.Fatal(err)
+	}
+	os.Remove(dstFile.Name())
+
+	err = unix.Fclonefileat(fd, unix.AT_FDCWD, dstFile.Name(), 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	clonedData, err := ioutil.ReadFile(dstFile.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(data, clonedData) {
+		t.Fatal(err)
+	}
 }
